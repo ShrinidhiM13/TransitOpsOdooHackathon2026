37d37dd1882076c868690afcf58e486f222b7040
ShrinidhiM13 <shrinidhim9168@gmail.com>
2026-07-12 11:12:42 +0530

    Integrate Gradle wrappers and resolve platform build configs

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.9.0")
 
     // Jetpack Compose
-    val composeBom = platform("androidx.compose:compose-bom:2024.05.02")
+    val composeBom = platform("androidx.compose:compose-bom:2024.06.00")
     implementation(composeBom)
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -84,7 +84,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.05.02"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.06.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
